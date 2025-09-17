721cb8ed74dbf3c4b408d3f81c92519f3f3e84f8
Shrutivang <shruti.angarkar@ibm.com>
2025-09-17 14:29:46 +0530

    Add files via upload

--- Selenium/Project/Activity3.java
+++ Selenium/Project/Activity3.java
@@ -1,52 +1,57 @@
 package Activities;
 
-import java.util.List;
-
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Activity3 {
-	WebDriver driver;
 	
-	 @BeforeClass
-	public void beforeClass() {
-		// Create a new instance of the Firefox driver
-		driver = new FirefoxDriver();
-		
-		// Open the browser
-		driver.get("https://training-support.net/webelements/login-form");
-		
-	}	
+	WebDriver driver;
+	WebDriverWait wait;
+
+	@BeforeClass
+	public void setup() {
+	driver = new FirefoxDriver();
+	wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	
+	// Open a browser.
+	// Navigate to ‘http://alchemy.hguy.co/orangehrm’
+	driver.get("http://alchemy.hguy.co/orangehrm");
+	}
 	
 	@Test
-	public void LoginTest() {
-		//   // Find the username and password fields
-		WebElement username = driver.findElement(By.id("username"));
-		WebElement password = driver.findElement(By.id("password"));
-		
-		// Enter credentials
-		username.sendKeys("admin");
-		password.sendKeys("password");
-		
-		// Click login
-		driver.findElement(By.xpath("//button[text()='Submit']")).click();
-		
-		// Read login message
-		String loginMessage = driver.findElement(By.cssSelector("h2.text-center")).getText();
-        Assert.assertEquals("A Simple Login Form!", loginMessage);
-		}
+	public void login(){
+	// Find and select the username and password fields
+	
+	WebElement username = driver.findElement(By.id("txtUsername"));
+	WebElement password = driver.findElement(By.id("txtPassword"));
+	
+	// Enter credentials
+	username.sendKeys("orange");
+	password.sendKeys("orangepassword123");
+	
+	// Click login
+	WebElement loginbutton = driver.findElement(By.id("btnLogin"));
+	loginbutton.click();
+			
+	// Read login message	
+	//Verify that the homepage has opened
+			
+	 String loginMessage = driver.findElement(By.className("firstLevelMenu")).getText();
+     Assert.assertEquals("Admin", loginMessage);
+			
+	}
 	
 	@AfterClass
-	public void afterclass() {
-		// Close browser
-		driver.close();
+	// Close the browser.
+	public void closing() {
+		driver.quit();
 	}
-
 }
